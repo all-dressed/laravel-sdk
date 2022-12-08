d97c09cc9541571f947eeb50994afb338c36339b
Chin Leung <chin@swoo.ca>
2022-12-08 15:46:18 -0500

    Added support for details of a given package

--- src/Builders/PackageBuilder.php
+++ src/Builders/PackageBuilder.php
@@ -3,8 +3,11 @@
 namespace AllDressed\Builders;
 
 use AllDressed\Client;
+use AllDressed\Exceptions\PackageNotFoundException;
 use AllDressed\Package;
+use Illuminate\Http\Client\RequestException;
 use Illuminate\Support\Collection;
+use Throwable;
 
 class PackageBuilder extends Builder
 {
@@ -19,11 +22,28 @@ class PackageBuilder extends Builder
 
         $endpoint = 'packages';
 
-        $response = $client->get($endpoint, [
-            'root' => $this->getOption('root'),
-        ]);
+        if ($id = $this->getOption('id')) {
+            $endpoint = "{$endpoint}/packages";
+        }
 
-        return collect($response->json())->mapInto(Package::class);
+        try {
+            $response = $client->get($endpoint, [
+                'root' => $this->getOption('root'),
+            ]);
+        } catch (RequestException $exception) {
+            $this->throw(
+                exception: $exception,
+                id: $id,
+            );
+        }
+
+        $data = $response->json('data');
+
+        if ($id) {
+            $data = [$data];
+        }
+
+        return collect($data)->mapInto(Package::class);
     }
 
     /**
@@ -34,5 +54,21 @@ class PackageBuilder extends Builder
     public function root(): static
     {
         return $this->withOption('root', true);
+    }
+
+    /**
+     * Throw a new friendly exception based on the existing exception.
+     *
+     * @param  \Throwable  $exception
+     * @param  string|null  $id
+     * @return void
+     */
+    protected function throw(Throwable $exception, string $id = null): void
+    {
+        if ($exception->getCode() == 404 && $id) {
+            throw new PackageNotFoundException($id, $exception);
+        }
+
+        throw $exception;
     }
 }
