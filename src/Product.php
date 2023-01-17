e0095a37ef387e5b193f06f9dd8506ed137c8fb2
Chin Leung <chin@swoo.ca>
2023-01-17 10:24:49 -0500

    Map the prices into a collection automatically

--- src/Product.php
+++ src/Product.php
@@ -3,9 +3,24 @@
 namespace AllDressed;
 
 use AllDressed\Builders\ProductBuilder;
+use Illuminate\Support\Arr;
 
 class Product extends Base
 {
+    /**
+     * Create a new package instance.
+     *
+     * @param  iterable<TKey, TValue>  $attributes
+     * @return void
+     */
+    public function __construct($attributes = [])
+    {
+        $attributes['prices'] = collect(Arr::get($attributes, 'prices', []))
+            ->mapInto(Price::class);
+
+        parent::__construct($attributes);
+    }
+
     /**
      * Create a new query builder.
      *
