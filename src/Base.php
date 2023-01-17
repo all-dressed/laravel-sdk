991fc940e934cec9e047aa6397f8f458620be104
Chin Leung <chin@swoo.ca>
2023-01-17 09:02:55 -0500

    Added methods to check if an attribute exists

--- src/Base.php
+++ src/Base.php
@@ -4,6 +4,7 @@ namespace AllDressed;
 
 use AllDressed\Builders\Builder;
 use AllDressed\Concerns\ForwardsToBuilder;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Fluent;
 
@@ -19,6 +20,28 @@ abstract class Base extends Fluent
     public static function all(): Collection
     {
         return static::query()->get();
+    }
+
+    /**
+     * Check if the instance has the given attribute.
+     *
+     * @param  string  $name
+     * @return bool
+     */
+    public function hasAttribute(string $name): bool
+    {
+        return Arr::has($this->attributes, $name);
+    }
+
+    /**
+     * Check if the instance is missing the given attribute.
+     *
+     * @param  string  $name
+     * @return bool
+     */
+    public function missingAttribute(string $name): bool
+    {
+        return ! $this->hasAttribute($name);
     }
 
     /**
