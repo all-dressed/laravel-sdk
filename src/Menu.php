66a7cef04cb9fb8ba7120fec6129192e4ef481cc
Chin Leung <chin@swoo.ca>
2023-02-28 18:01:27 -0500

    Casts dates to Carbon

--- src/Menu.php
+++ src/Menu.php
@@ -3,9 +3,30 @@
 namespace AllDressed;
 
 use AllDressed\Builders\MenuBuilder;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Carbon;
 
 class Menu extends Base
 {
+    /**
+     * Create a new menu instance.
+     *
+     * @param  iterable<TKey, TValue>  $attributes
+     * @return void
+     */
+    public function __construct($attributes = [])
+    {
+        if ($from = Arr::get($attributes, 'from')) {
+            Arr::set($attributes, 'from', Carbon::parse($from));
+        }
+
+        if ($to = Arr::get($attributes, 'to')) {
+            Arr::set($attributes, 'to', Carbon::parse($to));
+        }
+
+        parent::__construct($attributes);
+    }
+
     /**
      * Create a new query builder.
      *
