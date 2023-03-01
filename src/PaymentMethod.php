7203a9a266179389b6fc751028a0c81f57b2e480
Chin Leung <chin@swoo.ca>
2023-03-01 13:40:11 -0500

    Cast the address

--- src/PaymentMethod.php
+++ src/PaymentMethod.php
@@ -3,9 +3,25 @@
 namespace AllDressed;
 
 use AllDressed\Builders\PaymentMethodBuilder;
+use Illuminate\Support\Arr;
 
 class PaymentMethod extends Base
 {
+    /**
+     * Create a new payment method instance.
+     *
+     * @param  iterable<TKey, TValue>  $attributes
+     * @return void
+     */
+    public function __construct($attributes = [])
+    {
+        if ($address = Arr::get($attributes, 'address')) {
+            $attributes['address'] = new Address($address);
+        }
+
+        parent::__construct($attributes);
+    }
+
     /**
      * Create a new query builder.
      *
